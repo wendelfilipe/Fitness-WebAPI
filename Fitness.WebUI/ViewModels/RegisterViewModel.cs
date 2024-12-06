d198542098afb78c4655cab80fc5d93a124a9f67
wendelfilipe <wendelfilipe1997@hotmail.com>
2024-12-05 23:20:13 -0300

    try to fix login bug

--- Fitness.WebUI/ViewModels/RegisterViewModel.cs
+++ Fitness.WebUI/ViewModels/RegisterViewModel.cs
@@ -14,10 +14,12 @@ namespace Fitness.WebUI.ViewModels
         
         [Required]
         [DataType(DataType.Password)]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max" + "{1} characters long", MinimumLength = 10)]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
         public string ConfirmPassword { get; set; }
     }
 }
